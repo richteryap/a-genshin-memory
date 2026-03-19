5fc3917be49ecb3ead9cd41cea7c4cd275126e1d
richteryap <yap.richtera@gmail.com>
2026-03-19 15:28:21 +0800

    updated the look on home.jsx and added images

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      // Whenever our app requests /api/enka, Vite will forward it to enka.network
+      // Whenever app requests /api/enka, Vite will forward it to enka.network
       '/api/enka': {
         target: 'https://enka.network',
         changeOrigin: true,
